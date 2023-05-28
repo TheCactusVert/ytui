ccb54f0cee1b40a50f7d4bac57104ca87a47784c
TheCactusVert <kevin@gavrois.fr>
2023-05-28 21:16:43 +0200

    refactor: moving stuff

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,3 +1,7 @@
+mod videos;
+
+use videos::VideosList;
+
 use std::{error::Error, io};
 
 use crossterm::{
@@ -22,53 +26,6 @@ enum State {
     Search,
     List,
     Exit,
-}
-
-#[derive(Default)]
-struct VideosList<T> {
-    state: ListState,
-    items: Vec<T>,
-}
-
-impl<T> VideosList<T> {
-    fn with_items(items: Vec<T>) -> VideosList<T> {
-        VideosList {
-            state: ListState::default(),
-            items,
-        }
-    }
-
-    fn next(&mut self) {
-        let i = match self.state.selected() {
-            Some(i) => {
-                if i >= self.items.len() - 1 {
-                    0
-                } else {
-                    i + 1
-                }
-            }
-            None => 0,
-        };
-        self.state.select(Some(i));
-    }
-
-    fn previous(&mut self) {
-        let i = match self.state.selected() {
-            Some(i) => {
-                if i == 0 {
-                    self.items.len() - 1
-                } else {
-                    i - 1
-                }
-            }
-            None => 0,
-        };
-        self.state.select(Some(i));
-    }
-
-    fn unselect(&mut self) {
-        self.state.select(None);
-    }
 }
 
 #[derive(Default)]
---
+++ src/app/videos.rs
@@ -0,0 +1,72 @@
+use std::{error::Error, io};
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Text},
+    widgets::{canvas::Line, Block, Borders, List, ListItem, ListState, Paragraph},
+    Frame, Terminal,
+};
+use unicode_width::UnicodeWidthStr;
+
+#[derive(PartialEq, Default)]
+enum State {
+    #[default]
+    None,
+    Search,
+    List,
+    Exit,
+}
+
+#[derive(Default)]
+pub struct VideosList<T> {
+    pub state: ListState,
+    pub items: Vec<T>,
+}
+
+impl<T> VideosList<T> {
+    fn with_items(items: Vec<T>) -> VideosList<T> {
+        VideosList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn unselect(&mut self) {
+        self.state.select(None);
+    }
+}
